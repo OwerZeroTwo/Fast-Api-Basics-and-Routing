ed0aa3fcfe4a12d7a4ec405519b4657d4000796b
OwerZeroTwo <161704626+OwerZeroTwo@users.noreply.github.com>
2024-08-08 01:00:44 +0300

    module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -1,17 +1,39 @@
-from fastapi import FastAPI, Path
-from typing import Annotated
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+from typing import Optional
 
 app = FastAPI()
 
-@app.get("/user/{user_id}")
-async def read_user(
-    user_id: Annotated[int, Path(title="Enter User ID", description="User ID", ge=1, le=100, example=1)]
-):
-    return {"user_id": user_id}
+users = {'1': 'Имя: Example, возраст: 18'}
+last_id = 1
 
-@app.get("/user/{username}/{age}")
-async def read_user(
-    username: Annotated[str, Path(title="Enter username", description="Username", min_length=5, max_length=20, example="UrbanUser")],
-    age: Annotated[int, Path(title="Enter age", description="Age", ge=18, le=120, example=24)]
-):
-    return {"username": username, "age": age}
+@app.get("/users")
+async def get_users():
+    return JSONResponse(users)
+
+@app.post("/user/{username}/{age}")
+async def create_user(username: str, age: int):
+    global last_id
+    last_id += 1
+    users[str(last_id)] = f"Имя: {username}, возраст: {age}"
+    return JSONResponse(f"User {last_id} is registered")
+
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_user(user_id: int, username: str, age: int):
+    if str(user_id) in users:
+        users[str(user_id)] = f"Имя: {username}, возраст: {age}"
+        return JSONResponse(f"The user {user_id} is registered")
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
+
+@app.delete("/user/{user_id}")
+async def delete_user(user_id: int):
+    if str(user_id) in users:
+        del users[str(user_id)]
+        return JSONResponse(f"User {user_id} has been deleted")
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
